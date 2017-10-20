9a851b6442c29974620772234e54c335e5b0830f
MaestracciDamien <damien.maestracci@etu.univ-nantes.fr>
2017-10-20 11:17:22 +0200

    pikaboo

--- src/Controller/ControllerPrincipal.java
+++ src/Controller/ControllerPrincipal.java
@@ -1,4 +1,45 @@
 package Controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ControllerPrincipal {
+
+    private final String addr = "jdbc:mysql://127.0.0.1:8889/sgbd-cc";
+    private final String name = "root";
+    private final String password = "root";
+
+    private Connection conn;
+    public ControllerPrincipal() throws Exception {
+        //view = new TheView();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex1) {
+            System.out.println("Pilote non trouver");
+            System.exit(1);
+        }
+
+        try {
+            conn = DriverManager.getConnection(addr, name, password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reserverVisite(String nom, String prenom, int numSecu, int idAnalyse)
+    {
+
+    }
+
+
+    public void main(){
+        try {
+            ControllerPrincipal programme = new ControllerPrincipal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Model/Medecin.java
+++ src/Model/Medecin.java
@@ -1,4 +1,30 @@
 package Model;
 
-public class Medecin {
+public class Medecin extends Utilisateurs {
+    private int id;
+    private int salaire;
+
+    public Medecin(int id, int numeroSecu, String nom, String prenom, int id1, int salaire) {
+        super(id, numeroSecu, nom, prenom);
+        this.id = id1;
+        this.salaire = salaire;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalaire() {
+        return salaire;
+    }
+
+    public void setSalaire(int salaire) {
+        this.salaire = salaire;
+    }
 }
